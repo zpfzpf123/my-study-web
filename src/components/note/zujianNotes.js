ebf1ad537ce2b5d9b4be600a8a91b50eec1bd26c
18582297328 <2622013323@qq.com>
2023-02-08 16:26:49 +0800

    '增加笔记'

--- src/components/note/zujianNotes.js
+++ src/components/note/zujianNotes.js
@@ -4,7 +4,9 @@ export default [
         name:'1',
         content:'# 1.前端实用组件\n' +
             '\n' +
-            '## 1.1 [vue无缝滚动组件 vue2 ](https://chenxuan0000.github.io/vue-seamless-scroll/)  [vue3](https://doc.wssio.com/opensource/vue3-seamless-scroll/#%E5%BC%80%E5%A7%8B)\n' +
+            '## 1.1 [vue无缝滚动组件 vue2 ](https://chenxuan0000.github.io/vue-seamless-scroll/)  [vue3](https://doc.wssio.com/opensource/vue3-seamless-scroll/#%E5%BC%80%E5%A7%8B)\n'  +
+            '\n' +
+            '要想让他实现无缝滚动，首先看你页面的滚动内容高度是多少，比如是500px,在不滚动的时候看多少行数据占满这个高度，假设需要10行数据显示在页面上，那么每行的高度需要设置为大于500/10=50px,这样才可以实现无缝滚动，一般就设置为50px,通过v-if判断少于10行就不展示，多余10行就滚动\n'+
             '## 1.2 [vue-cropper裁剪图片插件](https://github.com/xyxiao001/vue-cropper)\n' +
             '## 1.3 [自定义表格](https://vxetable.cn/#/table/start/install)\n' +
             '## 1.4 [Vue 字符串实现JSON格式显示 ](https://github.com/chenfengjw163/vue-json-viewer/blob/master/README_CN.md)\n' +
